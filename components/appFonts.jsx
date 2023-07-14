dc9f328163901d7f1f7666db4410450b73bdbf11
omofolarin <folarinshonibare@yahoo.com>
2023-07-14 21:24:17 +0100

    fonts layer

---
+++ components/appFonts.jsx
@@ -0,0 +1,31 @@
+import { useFonts } from "expo-font";
+
+export const AppFonts = (props) => {
+  const { children } = props;
+  const [fontsLoaded] = useFonts({
+    "Montserrat-Black": require("../assets/fonts/Montserrat-Black.ttf"),
+    "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-Medium": require("../assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-BoldItalic": require("../assets/fonts/Montserrat-BoldItalic.ttf"),
+    "Montserrat-ExtraBold": require("../assets/fonts/Montserrat-ExtraBold.ttf"),
+    "Montserrat-ExtraBoldItalic": require("../assets/fonts/Montserrat-ExtraBoldItalic.ttf"),
+    "Montserrat-ExtraLight": require("../assets/fonts/Montserrat-ExtraLight.ttf"),
+    "Montserrat-ExtraLightItalic": require("../assets/fonts/Montserrat-ExtraLightItalic.ttf"),
+    "Montserrat-Italic": require("../assets/fonts/Montserrat-Italic.ttf"),
+    "Montserrat-Light": require("../assets/fonts/Montserrat-Light.ttf"),
+    "Montserrat-LightItalic": require("../assets/fonts/Montserrat-LightItalic.ttf"),
+    "Montserrat-Medium": require("../assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-MediumItalic": require("../assets/fonts/Montserrat-MediumItalic.ttf"),
+    "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    "Montserrat-Thin": require("../assets/fonts/Montserrat-Thin.ttf"),
+    "Montserrat-ThinItalic": require("../assets/fonts/Montserrat-ThinItalic.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
